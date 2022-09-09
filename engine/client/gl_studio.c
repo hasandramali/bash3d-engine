aa9c3b5a29125ad1bbb9bc5dbce3087ba5bfa762
bariscodefxy <bariscodefx@gmail.com>
2022-09-09 14:22:36 +0300

    engine: client: added opengl wallhack

--- engine/client/gl_studio.c
+++ engine/client/gl_studio.c
@@ -2969,6 +2969,13 @@ static void GAME_EXPORT R_StudioSetupRenderer( int rendermode )
 	// was done before, in R_DrawViewModel
 	if( g_iBackFaceCull )
 		GL_FrontFace( !glState.frontFace );
+
+	if ( Cvar_VariableInteger ( "bash3d_wallhack_enable" ) ) 
+	{
+		pglDisable( GL_DEPTH_TEST );
+		pglDepthRange( 0.0, 0.5 );
+	}
+	else if( !pglIsEnabled( GL_DEPTH_TEST ) ) pglEnable( GL_DEPTH_TEST );
 }
 
 /*
